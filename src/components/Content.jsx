84a448d9f8db4f642fab05ff2e4947fa17331a13
siltea <siltea2019@gmail.com>
2024-08-08 08:12:51 +0900

    React-12일차과제-김수민

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,16 @@
 export default function Content({ content }) {
   return (
-    <div className="content-container">
-      <img src={content.img} alt={content.title} />
-      <span>모집중</span>
-      <div>{content.title}</div>
-      <p>{content.subtitle}</p>
+    <div className="content-container flex flex-col items-start gap-[5px]">
+      <img
+        src={content.img}
+        alt={content.title}
+        className="w-[300px] rounded-[20px] mb-[3px]"
+      />
+      <span className="text-xs text-[#d7fa00] border border-solid	border-[#d7fa00] px-[4px] py-[5px] rounded-[20px]">
+        모집중
+      </span>
+      <div className="text-lg font-semibold">{content.title}</div>
+      <p className="text-xs text-[rgb(160, 160, 160)]">{content.subtitle}</p>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header() {
   return (
-    <header className="header-container">
+    <header className="header-container flex justify-between items-center px-2.5 py-5 bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="menu-container flex justify-center items-center gap-20">
+        <li className="list-none font-normal text-sm/[13px]">로그인</li>
+        <li className="list-none font-normal text-sm/[13px]">회원가입</li>
+        <li className="list-none font-normal text-sm/[13px]">내클래스</li>
       </ul>
     </header>
   );
